ece76fa5b9224ee90a63ddef2190ee2318daf1e0
Real man born on semtember <hieunam03@gmail.com>
2018-05-27 07:20:21 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@ LOCAL_PATH := device/samsung/fortuna3g
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := fortuna3g,fortuna3gxx
 
+# Legacy MMAP
+BOARD_USES_LEGACY_MMAP := true
+
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna3g_eur_defconfig
 
